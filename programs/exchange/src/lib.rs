fd7b0654e406bf9ba98bb22ed38c6b8b9c1c4c18
Pindaroso <me@pindaroso.com>
2021-09-11 17:43:36 -0400

    Updated liquidity function

--- programs/exchange/src/lib.rs
+++ programs/exchange/src/lib.rs
@@ -43,41 +43,31 @@ pub mod exchange {
             max_tokens_x > 0 && max_tokens_y > 0 && deadline > ctx.accounts.clock.unix_timestamp
         );
 
-        let authority = &mut ctx.accounts.authority;
         let exchange = &mut ctx.accounts.exchange;
+        if exchange.total_supply_y > 0 {
+            // eth_reserve: uint256(wei) = self.balance - msg.value
+            // token_reserve: uint256 = self.token.balanceOf(self)
+            // token_amount: uint256 = msg.value * token_reserve / eth_reserve + 1
+            // liquidity_minted: uint256 = msg.value * total_liquidity / eth_reserve
+            // assert max_tokens >= token_amount and liquidity_minted >= min_liquidity
+            // self.balances[msg.sender] += liquidity_minted
+            // self.totalSupply = total_liquidity + liquidity_minted
+            let y_reserve = exchange.total_supply_y - max_tokens_y;
+            let x_amount = max_tokens_y * exchange.total_supply_x / y_reserve + 1;
+            let liquidity_minted = max_tokens_y * exchange.total_supply_y / y_reserve;
+            assert!(max_tokens_x >= x_amount && liquidity_minted >= min_liquidity_x);
+            exchange.total_supply_x = exchange.total_supply_x + liquidity_minted;
+        } else {
+            // token_amount: uint256 = max_tokens
+            // initial_liquidity: uint256 = as_unitless_number(self.balance)  # `balance` is already defined
+            // self.totalSupply = initial_liquidity
+            // self.balances[msg.sender] = initial_liquidity
+            // assert self.token.transferFrom(msg.sender, self, token_amount)
+            let initial_liquidity = exchange.total_supply_y;
+            exchange.total_supply_x = initial_liquidity;
+            token::transfer(ctx.accounts.into_x_context(), max_tokens_x);
+        }
 
-        let from_x = &mut ctx.accounts.from_x;
-        let from_y = &mut ctx.accounts.from_y;
-
-        let total_liquidity_x = exchange.total_supply_x;
-
-        //if total_liquidity_x > 0 {
-        //    // eth_reserve: uint256(wei) = self.balance - msg.value
-        //    // token_reserve: uint256 = self.token.balanceOf(self)
-        //    // token_xmount: uint256 = msg.value * token_reserve / eth_reserve + 1
-        //    // liquidity_minted: uint256 = msg.value * total_liquidity / eth_reserve
-        //    // assert max_tokens >= token_amount and liquidity_minted >= min_liquidity
-        //    // self.balances[msg.sender] += liquidity_minted
-        //    // self.totalSupply = total_liquidity + liquidity_minted
-        //    let sol_reserve = exchange.balance - sol;
-        //    let token_reserve = exchange.total_supply;
-        //    let token_amount = sol * token_reserve / sol_reserve + 1;
-        //    let liquidity_minted = sol * total_liquidity / sol_reserve;
-        //    assert!(max_tokens >= token_amount && liquidity_minted >= min_liquidity);
-        //    exchange.total_supply = max_tokens;
-        //} else {
-        //    // token_amount: uint256 = max_tokens
-        //    // initial_liquidity: uint256 = as_unitless_number(self.balance)  # `balance` is already defined
-        //    // self.totalSupply = initial_liquidity
-        //    // self.balances[msg.sender] = initial_liquidity
-        //    // assert self.token.transferFrom(msg.sender, self, token_amount)
-        //    let token_amount = max_tokens;
-        //    let initial_liquidity = exchange.balance;
-        //    let total_supply = initial_liquidity;
-        //    exchange.total_supply = token_amount;
-        //}
-
-        token::transfer(ctx.accounts.into_x_context(), max_tokens_x);
         token::transfer(ctx.accounts.into_y_context(), max_tokens_y)
     }
 
