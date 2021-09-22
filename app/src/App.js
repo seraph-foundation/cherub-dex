31fa491bbc3f24827fe33c86a520bb7893533afa
Pindaroso <me@pindaroso.com>
2021-09-22 12:36:50 -0400

    Added initial skeleton for UI

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import 'antd/dist/antd.css';
 import './App.css';
 
-import { Button } from 'antd';
+import { Button, Card, Col, Layout, Menu, Row, Typography } from 'antd';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { useState } from 'react';
 import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
@@ -11,29 +11,25 @@ import { Connection, PublicKey } from '@solana/web3.js';
 
 import idl from './idl.json';
 
-const wallets = [getPhantomWallet()]
-
+const { Header, Footer, Content } = Layout;
+const { Title } = Typography;
 const { SystemProgram, Keypair } = web3;
+
+const wallets = [getPhantomWallet()]
 const baseAccount = Keypair.generate();
-const opts = {
-  preflightCommitment: "processed"
-}
+const opts = { preflightCommitment: "processed" };
 const programID = new PublicKey(idl.metadata.address);
 
 function App() {
-  const [value, setValue] = useState('');
-  const [dataList, setDataList] = useState([]);
-  const [input, setInput] = useState('');
+  const [menu, setMenu] = useState('swap');
   const wallet = useWallet()
 
   async function getProvider() {
     // Create the provider and return it to the caller network set to local network for now
     const network = "http://127.0.0.1:8899";
     const connection = new Connection(network, opts.preflightCommitment);
-
-    const provider = new Provider(
-      connection, wallet, opts.preflightCommitment,
-    );
+    const provider = new Provider(connection, wallet, opts.preflightCommitment);
+    console.log('provider', provider);
     return provider;
   }
 
@@ -54,65 +50,99 @@ function App() {
 
       const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
       console.log('account: ', account);
-      setValue(account.data.toString());
-      setDataList(account.dataList);
     } catch (err) {
       console.log("Transaction error: ", err);
     }
   }
 
-  async function update() {
-    if (!input) return
-    const provider = await getProvider();
-    const program = new Program(idl, programID, provider);
-    await program.rpc.update(input, {
-      accounts: {
-        baseAccount: baseAccount.publicKey
-      }
-    });
+  async function handleMenuClick(e) {
+    setMenu(e.key);
+  };
 
-    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('account: ', account);
-    setValue(account.data.toString());
-    setDataList(account.dataList);
-    setInput('');
-  }
+  async function onConnectWalletClick(e) {
+    document.getElementsByClassName('WalletMultiButton')[0].click();
+  };
 
-  if (!wallet.connected) {
-    return (
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
-        <WalletMultiButton />
-      </div>
-    )
-  } else {
-    return (
-      <div className="App">
-        <div>
-          {
-            !value && (<Button type="primary" onClick={initialize}>Initialize</Button>)
-          }
-          {
-            value ? (
-              <div>
-                <h2>Current value: {value}</h2>
-                <input
-                  placeholder="Add new data"
-                  onChange={e => setInput(e.target.value)}
-                  value={input}
-                />
-                <button onClick={update}>Add data</button>
+  return (
+    <Layout className="App">
+      <Header className="Header">
+        <Row>
+          <Col span={3}>
+            <div className="logo"><strong>xv01.finance</strong></div>
+          </Col>
+          <Col span={13}>
+            <Menu className="Menu" onClick={handleMenuClick} selectedKeys={[menu]} mode="horizontal">
+              <Menu.Item key="swap">Swap</Menu.Item>
+              <Menu.Item key="pool">Pool</Menu.Item>
+              <Menu.Item key="charts">Charts</Menu.Item>
+            </Menu>
+          </Col>
+          <Col span={8} className="ConnectWalletHeader">
+            { !wallet.connected ? (
+              <>
+                <WalletMultiButton className="WalletMultiButton" />
+                <Button onClick={onConnectWalletClick} type="link">Connect Wallet</Button>
+              </>) :
+              <div className="Connected">
+                <code>
+                  { wallet.publicKey.toString().substr(0, 4) }...{ wallet.publicKey.toString().substr(-4) }
+                </code>
               </div>
-            ) : (
-              <h3>Please Initialize.</h3>
-            )
-          }
-          {
-            dataList.map((d, i) => <h4 key={i}>{d}</h4>)
-          }
+            }
+          </Col>
+        </Row>
+      </Header>
+      <Content>
+        <div>
+          <br/>
+          <br/>
+          { !wallet.connected ? (
+            <>
+              <Title>Perpetual futures and yield-based XV01 pooling protocol</Title>
+              <Row>
+                <Col span={12}>
+                  <Button className="ConnectWallet" type="primary" size="large">Connect Wallet</Button>
+                </Col>
+                <Col span={12}>
+                  <Button className="LearnMore" ghost size="large">Learn More</Button>
+                </Col>
+              </Row>
+            </>
+          ) : <Title level={2}>Balance: 0 SOL</Title> }
+          <br/>
+          <br/>
+          <Row>
+            <Col span={6}></Col>
+            <Col span={12} className="Cards">
+              { menu === "swap" ? (
+                <div className="site-card-border-less-wrapper">
+                  <Card title="Swap" bordered={false}>
+                    <p>Card content</p>
+                  </Card>
+                </div>
+              ) : "" }
+              { menu === "pool" ? (
+                <div className="site-card-border-less-wrapper">
+                  <Card title="Pool" bordered={false}>
+                    <p>Card content</p>
+                  </Card>
+                </div>
+              ) : "" }
+              { menu === "charts" ? (
+                <div className="site-card-border-less-wrapper">
+                  <Card title="Charts" bordered={false}>
+                    <p>Card content</p>
+                  </Card>
+                </div>
+              ) : "" }
+            </Col>
+            <Col span={6}></Col>
+          </Row>
         </div>
-      </div>
-    );
-  }
+      </Content>
+      <Footer><code className="CurrentBlock"><small>97,826,670</small></code></Footer>
+    </Layout>
+  );
 }
 
 const AppWithProvider = () => (
