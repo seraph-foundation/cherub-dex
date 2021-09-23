bb93792340d1d19aea9d76e520d9c52840f7fef3
Pindaroso <me@pindaroso.com>
2021-09-23 10:23:29 -0400

    Refactoring

--- programs/pyth/src/lib.rs
+++ programs/pyth/src/lib.rs
@@ -20,26 +20,21 @@ pub mod pyth {
         let pyth_product = pyth_client::cast::<pyth_client::Product>(pyth_product_data);
 
         if pyth_product.magic != pyth_client::MAGIC {
-            msg!("Pyth product account provided is not a valid Pyth account");
-            return Err(ProgramError::InvalidArgument.into());
+            return Err(ErrorCode::ValidPythAccount.into());
         }
         if pyth_product.atype != pyth_client::AccountType::Product as u32 {
-            msg!("Pyth product account provided is not a valid Pyth product account");
-            return Err(ProgramError::InvalidArgument.into());
+            return Err(ErrorCode::ValidProductAccount .into());
         }
         if pyth_product.ver != pyth_client::VERSION_2 {
-            msg!("Pyth product account provided has a different version than the Pyth client");
-            return Err(ProgramError::InvalidArgument.into());
+            return Err(ErrorCode::PythClientVersion.into());
         }
         if !pyth_product.px_acc.is_valid() {
-            msg!("Pyth product price account is invalid");
-            return Err(ProgramError::InvalidArgument.into());
+            return Err(ErrorCode::InvalidProductAccount.into());
         }
 
         let pyth_price_pubkey = Pubkey::new(&pyth_product.px_acc.val);
         if &pyth_price_pubkey != pyth_price_info.key {
-            msg!("Pyth product price account does not match the Pyth price provided");
-            return Err(ProgramError::InvalidArgument.into());
+            return Err(ErrorCode::ProductPriceAccount.into());
         }
 
         // Put data in caller's account data
@@ -89,4 +84,18 @@ pub struct GetPrice<'info> {
 #[account]
 pub struct PythData {
     pub data: u64,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Pyth product account provided is not a valid Pyth account")]
+    ValidPythAccount,
+    #[msg("Pyth product account provided is not a valid Pyth product account")]
+    ValidProductAccount,
+    #[msg("Pyth product account provided has a different version than the Pyth client")]
+    PythClientVersion,
+    #[msg("Pyth product price account is invalid")]
+    InvalidProductAccount,
+    #[msg("Pyth product price account does not match the Pyth price provided")]
+    ProductPriceAccount,
 }
