dd2c80b67526b73d7b710c3b1ecbea42c7b55856
Pindaroso <me@pindaroso.com>
2021-10-19 05:17:12 -0400

    Fixed Docusaurus broken links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs/intro',
+                to: '/docs/home',
               },
             ],
           },
