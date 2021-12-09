0eab6132222a71d5ee27ac9797670fc2ca1457d3
pindaroso <me@pindaroso.com>
2021-12-09 11:31:03 -0500

    Added bot for setting oracle stub data

---
+++ bots/oracle.js
@@ -0,0 +1,40 @@
+const { FTXUS } = require('ftx-us');
+const { BN, TOKEN_PROGRAM_ID, PublicKey, Token, init, sleep, toBuffer } = require('../sdk/src')
+
+const config = init()
+
+async function main() {
+  console.log('Running....')
+
+  // Key and secret are needed for market data feed
+  const ftxUS = new FTXUS({ key: process.env.FTX_API_KEY, secret: process.env.FTX_API_SECRET })
+
+  while (true) {
+    const markets = await ftxUS.Markets.list()
+
+    for (let i = 0; i < config.accounts.exchanges.length; i++) {
+      const oraclePk = new PublicKey(config.accounts.exchanges[i].oracle)
+      const tokenPk = new PublicKey(config.accounts.exchanges[i].tokenV)
+
+      const tokenV = new Token(config.provider.connection, tokenPk, TOKEN_PROGRAM_ID, config.provider.wallet.payer)
+      const mintInfoV = await tokenV.getMintInfo()
+
+      const market = markets.filter(market => market.name === config.accounts.exchanges[i].symbol + '/USD')[0]
+      const price = market.price * (10 ** mintInfoV.decimals)
+
+      console.log('Setting token', tokenPk.toString(), 'oracle', oraclePk.toString(), 'price to', price.toString() + '...')
+
+      const tx = await config.programs.pyth.rpc.setPrice(new BN(price), {
+        accounts: {
+          price: oraclePk
+        }
+      })
+
+      console.log('Your transaction signature', tx)
+    }
+
+    await sleep(1000)
+  }
+}
+
+main()
---
+++ docs/docs/dao.md
@@ -0,0 +1,19 @@
+---
+sidebar_position: 2
+---
+
+# DAO
+
+The Cherub Protocol needs funds to initialize the insurance fund as well as pay developers. Minting tokens, distributing them to the team, and a treasury diversification round for the DAO is the ideal framework accomplishing this as a decentralized entity. Swaps will be done with DAOs, venture capitalists and mercenary capital, in that preferred order.
+
+## Vision
+
+The Cherub DAO mission is to create the best perpetuals DEX for retail users.
+
+## Strategy
+
+We believe quoting perps in USD (inverse perps), no trade minimums or maximums, and low fees are the optimal way to position ourselves in such a competitive landscape.
+
+## Tactics
+
+This section should get into the finer details of the protocol unique selling point (USP). Here we want to answer questions like:
